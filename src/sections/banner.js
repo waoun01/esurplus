73be48a67adf04b5d80a0b3b559754c675a661d4
waoun01 <108688185+waoun01@users.noreply.github.com>
2022-07-22 01:26:07 +0300

    Update banner.js

--- src/sections/banner.js
+++ src/sections/banner.js
@@ -22,7 +22,7 @@ export default function Banner() {
 <input type="image" src="https://www.paypalobjects.com/en_US/GB/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
 <img alt="" border="0" src="https://www.paypal.com/en_GB/i/scr/pixel.gif" width="1" height="1" />
 </form>*/}
-          <Button variant ="primary">E-Surplus Seminars</Button>
+          
         </Box>
 
       </Container>
