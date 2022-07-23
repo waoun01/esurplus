cd7c773a011cfc3d40583aabe51b2378c455ceb9
waoun01 <108688185+waoun01@users.noreply.github.com>
2022-07-23 16:11:30 +0300

    Update seminars.js

--- src/components/seminars.js
+++ src/components/seminars.js
@@ -1,7 +1,10 @@
 import { Container, Box, Heading, Text, Image, Button } from 'theme-ui';
+import Link from 'next/link';
 
 const Seminars = () => {
-    return <Button variant="primary">E-Surplus Seminars</Button>
+    return (
+        <Link href="/e-seminars"><Button variant="primary">E-Surplus Seminars</Button></Link>
+        )
 }
 
 export default Seminars;
