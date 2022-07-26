adc3177dc571e8fb96fe4fa43ddb6e16a83731cd
waoun01 <108688185+waoun01@users.noreply.github.com>
2022-07-27 01:28:58 +0300

    Update e-seminars.js

--- src/pages/e-seminars.js
+++ src/pages/e-seminars.js
@@ -1,9 +1,30 @@
-const About = () => {
-    return(
+/** @jsx jsx */
+import { jsx } from 'theme-ui';
+import ESem from 'assets/newimg.png';
+import { Container, Box, Heading, Text, Image, Button } from 'theme-ui';
+
+
+
+export default function About() {
+    return (
+        
         <>
-            <h1>Yo</h1>
-        </>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+                <Image
+                    src={ESem}
+                    alt="E-Surplus Seminars"
+                    layout="fill"
+                    objectFit="cover"
+                    style={{
+                        margin: "auto",
+                    }}
+                />
+      </div>
+    </>
     )
 }
-
-export default About;
